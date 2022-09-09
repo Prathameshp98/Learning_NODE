e92c2c752bef53e6ca1feff5ba9800c3bc07f100
Prathamesh Patil <prathamesh.patil@ufaber.com>
2022-09-09 12:25:35 +0530

    app-third : added routers

--- App-third/app.js
+++ App-third/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}))
@@ -10,20 +13,12 @@ app.use(bodyParser.urlencoded({extended: false}))
 //     next();   // helps to move to next middleware
 // });
 
-app.use('/add-product', (req, res, next) => {
-  // console.log('In another middleware!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">ADD PRODUCT</button></form>')
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect("/")
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
 })
-
-app.use('/', (req, res, next) => {
-   // console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
 
 app.listen(3000, () => {
     console.log("App started on port 3000")
