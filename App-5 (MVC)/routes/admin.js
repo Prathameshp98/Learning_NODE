ea5f9c711c8598533eac3ff1b83fabf8fb1630fa
Prathamesh Patil <prathamesh.patil@ufaber.com>
2022-10-11 15:08:28 +0530

    initial

---
+++ App-5 (MVC)/routes/admin.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path')
+
+
+
+const router = express.Router()
+
+const products = []
+
+router.get('/add-product', (req, res, next) => {
+
+    res.render('add-product' ,{
+      path: '/admin/add-product', 
+      pageTitle: 'Add Product',
+      isAddproductActive: true,
+      productCSS: true,
+      formCSS: true
+    })
+    
+  });
+  
+router.post('/add-product', (req, res, next) => {
+    products.push({title: req.body.title})
+    res.redirect("/")
+})
+
+exports.routes = router
+exports.products = products
