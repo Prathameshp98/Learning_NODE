65ac94fc17a6e42bdfe3cd44a913003027af916d
Prathamesh Patil <prathamesh.patil@ufaber.com>
2022-10-11 09:41:30 +0530

    handlebars and pug

--- App-4 (EJS)/routes/admin.js
+++ App-4 (EJS)/routes/admin.js
@@ -5,14 +5,22 @@ const rootDirectory = require('../util/path')
 
 const router = express.Router()
 
+const products = []
+
 router.get('/add-product', (req, res, next) => {
     // console.log('In another middleware!');
-    res.sendFile(path.join(rootDirectory, 'views', 'add-product.html'))
+    // res.sendFile(path.join(rootDirectory, 'views', 'add-product.html'))
+    res.render('add-product' ,{
+      path: '/admin/add-product', 
+      pageTitle: 'Add Product'
+    })
+    
   });
   
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body)
+    products.push({title: req.body.title})
     res.redirect("/")
 })
 
-module.exports = router;
+exports.routes = router
+exports.products = products
--- App-4 (EJS)/routes/shop.js
+++ App-4 (EJS)/routes/shop.js
@@ -2,12 +2,21 @@ const express = require('express');
 const path = require('path')
 
 const rootDirectory = require('../util/path')
+const adminData = require('./admin')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    console.log(adminData.products)
     // console.log('In another middleware!');
-   res.sendFile(path.join(rootDirectory, 'views', 'shop.html'))
+//    res.sendFile(path.join(rootDirectory, 'views', 'shop.html'))
+   res.render('shop', {
+        prods: adminData.products, 
+        path: '/', 
+        pageTitle: 'Shop',
+        hasProducts: adminData.products.length > 0
+    })
+    
 });
  
 module.exports = router;
