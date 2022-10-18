391d3f438c079391c67fcca187ae59c65cb3bf5f
Prathamesh Patil <prathamesh.patil@ufaber.com>
2022-10-18 23:37:42 +0530

    final commit

--- App-5 (MVC)/controllers/products.js
+++ App-5 (MVC)/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../model/product')
 
 exports.getAddProduct = (req, res, next) => {
 
@@ -13,17 +13,20 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({title: req.body.title})
+    const product = new Product(req.body.title)
+    product.save();
     res.redirect("/")
 }
 
 
 exports.getShop =(req, res, next) => {
-
-    res.render('shop', {
-         prods: products, 
-         path: '/', 
-         pageTitle: 'Shop',
+     Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products, 
+            path: '/', 
+            pageTitle: 'Shop',
+        })
      })
+    
      
  }
