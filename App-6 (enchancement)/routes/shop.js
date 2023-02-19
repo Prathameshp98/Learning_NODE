16c8a0fee85a53fbaa1e3d7089affb97b2be7a9c
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-02-19 11:57:16 +0530

    initial

---
+++ App-6 (enchancement)/routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path')
+
+const router = express.Router();
+
+const productController = require('../controllers/products')
+
+router.get('/', productController.getShop);
+ 
+module.exports = router;
