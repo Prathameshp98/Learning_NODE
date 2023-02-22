4b3dcaf7b7d32a7581b6349dfc34ffec8791b736
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-02-22 20:06:31 +0530

    added swagger docs to backend

--- App-6 (enchancement)/app.js
+++ App-6 (enchancement)/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path')
+const fs = require('fs')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+const customCss = fs.readFileSync((process.cwd()+"/swagger.css"), 'utf8');
+
 
 const app = express();
 
@@ -11,6 +17,8 @@ app.set('views','views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCss}));
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
